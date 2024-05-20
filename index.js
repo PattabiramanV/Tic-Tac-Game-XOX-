a06acfed215360fafa951290c3acc1e455f77932
PattabiramanV <pattabiramanvdckap@gmail.com>
2024-05-20 21:32:26 +0530

    add

--- index.js
+++ index.js
@@ -3,182 +3,189 @@ let popupRef = document.querySelector(".popup");
 let newgameBtn = document.getElementById("new-game");
 let restartBtn = document.getElementById("restart");
 let msgRef = document.getElementById("message");
-let allBtns=document.querySelector(".wrapper");
+let allBtns = document.querySelector(".wrapper");
 const cells = document.querySelectorAll('.button-option');
-console.log(cells);
-        let currentPlayer = 'X';
-        const board = Array(9).fill(null);
+let notificationSound = document.querySelector("#notificationSound");
+let currentPlayer = 'X';
+const board = Array(9).fill(false);
 
-        const winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-
-
-        function checkWinner() {
-            for (let combination of winningCombinations) {
-                const [a, b, c] = combination;
-                if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-                    return board[a];
-                }
-            }
-            return null;
-        }
-
-        function handleClick(clickIndex) {
-// console.log(index);
-// console.log(event.target);
-            const cell = event.target;
-            const index = clickIndex;
-
-            if (board[index] || checkWinner()) {
-
-                console.log("ji");
-                return;
-            }
-
-            board[index] = currentPlayer;
-            cell.textContent = currentPlayer;
-   
-          // if(!board.includes(null)){
-          //   drawFunction();
-          //   allBtns.style.display='none';
-          // }
-
-            const winner = checkWinner();
-            if (winner) {
-              
-              setTimeout(()=>{
-                msgRef.innerHTML = `&#x1F389; <br> ${winner} Wins`;
-              allBtns.style.dispaly='none';
-              popupRef.classList.remove("hide");
-              },500)
-                return;
-            }
-        if(!board.includes(null)){
-          allBtns.style.dispaly='none';
-              popupRef.classList.remove("hide");
-          msgRef.innerHTML = "&#x1F60E; <br> It's a Draw";
-        }
-            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-        }
+const winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
 
-        cells.forEach((cell,index) => {
+function checkWinner() {
+  for (let combination of winningCombinations) {
+    const [a, b, c] = combination;
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
+  return null;
+}
 
-          cell.addEventListener('click', ()=>{
+function handleClick(clickIndex) {
 
-            handleClick(index);
-          })
-        
+  const cell = event.target;
+  const index = clickIndex;
 
-        });
+  if (board[index] || checkWinner()) {
+
+    console.log("ji");
+    return;
+  }
+
+  board[index] = currentPlayer;
+  cell.innerHTML = currentPlayer;
+
+  if (cell.innerHTML != "X") {
+    cell.style.color = "rgb(205, 24, 24)";
+  }
+  notificationSound.play();
+  const winner = checkWinner();
+  if (winner) {
+
+    setTimeout(() => {
+      msgRef.innerHTML = `&#x1F389; <br> ${winner} Wins`;
+      allBtns.style.display = 'none';
+      popupRef.classList.remove("hide");
+    // Draw();
+
+    }, 500)
+    return;
+  }
+  if (!board.includes(false)) {
+    allBtns.style.dispaly = 'none';
+    popupRef.classList.remove("hide");
+    msgRef.innerHTML = "&#x1F60E; <br> It's a Draw";
+  // Draw();
+
+  }
+  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+
+}
 
 
-// winningShowFun()
-// function winningShowFun(){
+cells.forEach((cell, index) => {
 
-// }
+  cell.addEventListener('click', () => {
+
+    handleClick(index);
+  })
 
 
-// //Winning Pattern Array
-// let winningPattern = [
-//   [0, 1, 2],
-//   [0, 3, 6],
-//   [2, 5, 8],
-//   [6, 7, 8],
-//   [3, 4, 5],
-//   [1, 4, 7],
-//   [0, 4, 8],
-//   [2, 4, 6],
-// ];
-// //Player 'X' plays first
+});
 
-// let xTurn = true;
-// let count = 0;
 
-// //Disable All Buttons
-// const disableButtons = () => {
-//   btnRef.forEach((element) => (element.disabled = true));
-//   //enable popup
-//   popupRef.classList.remove("hide");
-// };
 
-// //Enable all buttons (For New Game and Restart)
-// const enableButtons = () => {
-//   btnRef.forEach((element) => {
-//     element.innerText = "";
-//     element.disabled = false;
-//   });
-//   //disable popup
-//   popupRef.classList.add("hide");
-// };
 
-// //This function is executed when a player wins
-// const winFunction = (letter) => {
-//   disableButtons();
-//   if (letter == "X") {
-//     msgRef.innerHTML = "&#x1F389; <br> 'X' Wins";
-//   } else {
-//     msgRef.innerHTML = "&#x1F389; <br> 'O' Wins";
-//   }
-// };
 
-// // Function for draw
-// const drawFunction = () => {
-//   disableButtons();
-//   msgRef.innerHTML = "&#x1F60E; <br> It's a Draw";
-// };
 
-// //New Game
-// newgameBtn.addEventListener("click", () => {
-//   count = 0;
-//   enableButtons();
-// });
-// restartBtn.addEventListener("click", () => {
-//   count = 0;
-//   enableButtons();
-// });
 
-// //Win Logic
-// const winChecker = () => {
-//   //Loop through all win patterns
-//   for (let i of winningPattern) {
-//     let [element1, element2, element3] = [
-//       btnRef[i[0]].innerText,
-//       btnRef[i[1]].innerText,
-//       btnRef[i[2]].innerText,
-//     ];
-//     //Check if elements are filled
-//     //If 3 empty elements are same and would give win as would
-//     if (element1 != "" && (element2 != "") & (element3 != "")) {
-//       if (element1 == element2 && element2 == element3) {
-//         //If all 3 buttons have same values then pass the value to winFunction
-//         winFunction(element1);
-//       }
-//     }
-//   }
-// };
 
-// //Display X/O on click
-// btnRef.forEach((element) => {
-//   element.addEventListener("click", () => {
-//     if (xTurn) {
-//       xTurn = false;
-//       //Display X
-//       element.innerText = "X";
-//       element.disabled = true;
-//     } else {
-//       xTurn = true;
-//       //Display Y
-//       element.innerText = "O";
-//       element.disabled = true;
-//     }
-//     //Increment count on each click
-//     count += 1;
-//     if (count == 9) {
-//       drawFunction();
-//     }
-//     //Check for win on every click
-//     winChecker();
-//   });
-// });
-// //Enable Buttons and disable popup on page load
-// window.onload = enableButtons;
+
+
+
+//..................................color paper.........................//
+
+
+let W = window.innerWidth;
+let H = window.innerHeight;
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const maxConfettis = 150;
+const particles = [];
+
+const possibleColors = [
+  "DodgerBlue",
+  "OliveDrab",
+  "Gold",
+  "Pink",
+  "SlateBlue",
+  "LightBlue",
+  "Gold",
+  "Violet",
+  "PaleGreen",
+  "SteelBlue",
+  "SandyBrown",
+  "Chocolate",
+  "Crimson"
+];
+
+function randomFromTo(from, to) {
+  return Math.floor(Math.random() * (to - from + 1) + from);
+}
+
+function confettiParticle() {
+  this.x = Math.random() * W; // x
+  this.y = Math.random() * H - H; // y
+  this.r = randomFromTo(11, 33); // radius
+  this.d = Math.random() * maxConfettis + 11;
+  this.color =
+    possibleColors[Math.floor(Math.random() * possibleColors.length)];
+  this.tilt = Math.floor(Math.random() * 33) - 11;
+  this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
+  this.tiltAngle = 0;
+
+  this.draw = function() {
+    context.beginPath();
+    context.lineWidth = this.r / 2;
+    context.strokeStyle = this.color;
+    context.moveTo(this.x + this.tilt + this.r / 3, this.y);
+    context.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5);
+    return context.stroke();
+  };
+}
+
+function Draw() {
+  const results = [];
+// alert("hi")
+  // Magical recursive functional love
+  requestAnimationFrame(Draw);
+
+  context.clearRect(0, 0, W, window.innerHeight);
+
+  for (var i = 0; i < maxConfettis; i++) {
+    results.push(particles[i].draw());
+  }
+
+  let particle = {};
+  let remainingFlakes = 0;
+  for (var i = 0; i < maxConfettis; i++) {
+    particle = particles[i];
+
+    particle.tiltAngle += particle.tiltAngleIncremental;
+    particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
+    particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
+
+    if (particle.y <= H) remainingFlakes++;
+
+    // If a confetti has fluttered out of view,
+    // bring it back to above the viewport and let if re-fall.
+    if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
+      particle.x = Math.random() * W;
+      particle.y = -30;
+      particle.tilt = Math.floor(Math.random() * 10) - 20;
+    }
+  }
+
+  return results;
+}
+
+window.addEventListener(
+  "resize",
+  function() {
+    W = window.innerWidth;
+    H = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  },
+  false
+);
+
+// Push new confetti objects to `particles[]`
+for (var i = 0; i < maxConfettis; i++) {
+  particles.push(new confettiParticle());
+}
+
+// Initialize
+canvas.width = W;
+canvas.height = H;
